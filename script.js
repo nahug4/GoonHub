b582055087787cd95f48402e33d171261d8ccce3
Guhan <564112@my.cuhsd.org>
2025-04-19 22:24:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// button to caleb page
 document.addEventListener("DOMContentLoaded", function () {
   const calebButton = document.getElementById("caleb");
 
   calebButton.addEventListener("click", function () {
     window.location.href = "caleb.html";
   });
+  
+//button to kye page
+  document.addEventListener("DOMContentLoaded", function () {
+  const calebButton = document.getElementById("kye");
+
+  calebButton.addEventListener("click", function () {
+    window.location.href = "kye.html";
+  });
+
+    
 }); // sigma
 function openFullscreen(imgElement) {
   if (imgElement.requestFullscreen) {
