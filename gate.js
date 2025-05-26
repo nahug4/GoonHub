1c751c1e693b22aa3bf2633e734abe909e451c28
Guhan <564112@my.cuhsd.org>
2025-05-26 12:56:08 -0700

    Update gate.js

--- gate.js
+++ gate.js
@@ -1,11 +1,11 @@
-const correctCode = "mySecret123"; // Replace this with your actual code
+const correctCode = "calebhasagyatt"; 
 
 document.getElementById("submitBtn").addEventListener("click", function () {
   const userInput = document.getElementById("codeInput").value;
   const errorMsg = document.getElementById("error");
 
   if (userInput === correctCode) {
-    // Redirect to your real homepage
+
     window.location.href = "home.html";
   } else {
     errorMsg.textContent = "Incorrect code. Try again.";
